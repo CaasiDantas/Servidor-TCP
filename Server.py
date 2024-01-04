ff89b965b74a88bdaef5206b035152cc286cf94d
Isaac Dantas <153383825+CaasiDantas@users.noreply.github.com>
2024-01-04 20:50:57 -0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,34 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+porta = int(input("Digite a porta para a conexão: "))
+
+try:
+
+    server.bind(("0.0.0.0", porta))
+    server.listen(5)
+    print("Esperando a conexão...")
+    client_socket, address = server.accept()
+    print("Conectado com o IP: " + address[0])
+
+    while True:
+
+        pacotes_recebidos = client_socket.recv(100000).decode()
+        print(f"Mensagem do Cliente: {pacotes_recebidos}")
+        
+        if pacotes_recebidos == "tchau\n":
+            print("Fim da conexão")
+            break
+        
+        mensagem_servidor = input("Mensagem do Servidor: ") + "\n"
+        client_socket.send(mensagem_servidor.encode()) 
+        
+        if mensagem_servidor == "tchau\n":
+            print("Fim da conexão")
+            break
+
+    client_socket.close()
+
+except Exception as error:
+    print("Erro: ", error)
